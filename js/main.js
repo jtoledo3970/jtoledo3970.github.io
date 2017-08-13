8f2b5e3d8cd875e14245569bd68923bc88332a68
jtoledo3970 <jtoledo3970@gmail.com>
2017-08-13 01:27:23 -0700

    Fixed css
    Colors were changed
    Removed unused sections
    Filled in services
    Fixed counter

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-/* 
+
+/*
    CounterUp
    ========================================================================== */
     jQuery(document).ready(function( $ ) {
@@ -7,14 +8,14 @@
       });
     });
 
-/* 
+/*
    MixitUp
    ========================================================================== */
     $(function(){
       $('#portfolio').mixItUp();
     });
 
-/* 
+/*
    Touch Owl Carousel
    ========================================================================== */
     $(".touch-slider").owlCarousel({
@@ -31,7 +32,7 @@
     $('.touch-slider').find('.owl-prev').html('<i class="fa fa-chevron-left"></i>');
     $('.touch-slider').find('.owl-next').html('<i class="fa fa-chevron-right"></i>');
 
-/* 
+/*
    Sticky Nav
    ========================================================================== */
     $(window).on('scroll', function() {
@@ -42,7 +43,7 @@
         }
     });
 
-/* 
+/*
    VIDEO POP-UP
    ========================================================================== */
     $('.video-popup').magnificPopup({
@@ -54,7 +55,7 @@
         fixedContentPos: false,
     });
 
-/* 
+/*
    Back Top Link
    ========================================================================== */
     var offset = 200;
@@ -74,21 +75,21 @@
       return false;
     })
 
-/* 
+/*
    One Page Navigation & wow js
    ========================================================================== */
   jQuery(function($) {
       //Initiat WOW JS
       new WOW().init();
 
-      // one page navigation 
+      // one page navigation
       $('.main-navigation').onePageNav({
               currentClass: 'active'
-      });    
+      });
   });
 
   jQuery(document).ready(function() {
-     
+
       $('body').scrollspy({
           target: '.navbar-collapse',
           offset: 195
@@ -106,7 +107,7 @@
 
   /* Nivo Lightbox
   ========================================================*/
-  jQuery(document).ready(function( $ ) {    
+  jQuery(document).ready(function( $ ) {
      $('.lightbox').nivoLightbox({
       effect: 'fadeScale',
       keyboardNav: true,
@@ -124,7 +125,7 @@
     });
   });
 
-/* 
+/*
    Page Loader
    ========================================================================== */
    $(window).load(function() {
